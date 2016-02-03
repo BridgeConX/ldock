084887e2f9aa32641c2e7c1daba6373382799fb7
Adam Engebretson <adam.engebretson@bridgeconx.com>
2016-02-02 23:43:38 -0600

    Adding some more cool sugar
    Signed-off-by: Adam Engebretson <adam.engebretson@bridgeconx.com>

--- phpnginx/files/etc/my_init.d/100_clone_application.sh
+++ phpnginx/files/etc/my_init.d/100_clone_application.sh
@@ -3,26 +3,42 @@
 echo "==> Setting COMPOSER_HOME /root/.composer"
 export COMPOSER_HOME=/root/.composer
 
-if [ $GITHUB_TOKEN ]; then
+if [ "$GITHUB_TOKEN" ]; then
     echo "==> Setting GitHub Access Token"
     composer config -g github-oauth.github.com $GITHUB_TOKEN
 fi
 
-if [ $GIT_URL ]; then
-  echo "==> Found git URL: $GIT_URL"
-  echo "==> Cleaning /var/www"
-  rm -rf /var/www/* /var/www/.*
-  echo "==> Cloning Application into /var/www"
-  git clone $GIT_URL /var/www
+if [ "$ID_RSA" ]; then
+    echo "==> Found id_rsa, writing..."
+    echo "$ID_RSA" >> /root/.ssh/id_rsa
+    chmod 600 /root/.ssh/id_rsa
+fi
 
-  if [ "$COMPOSER_INSTALL" != "0" ]; then
-    echo "==> Running 'composer install --no-dev'"
-    composer install --no-dev
-  fi
+if [ "$GIT_URL" ]; then
+    if [ "$PRE_INSTALL" ]; then
+        echo "==> Running PRE_INSTALL"
+        sh -c "$PRE_INSTALL"
+    fi
 
-  if [ "$STORAGE_WRITABLE" != "0" ]; then
-      echo "==> Attempting to make storage directory writable"
-      chmod -R 777 storage
-  fi
+    echo "==> Found git URL: $GIT_URL"
+    echo "==> Cleaning /var/www"
+    rm -rf /var/www/* /var/www/.*
+    echo "==> Cloning Application into /var/www"
+    git clone $GIT_URL /var/www
+
+    if [ "$COMPOSER_INSTALL" != "0" ]; then
+        echo "==> Running 'composer install --no-dev'"
+        composer install --no-dev
+    fi
+
+    if [ "$STORAGE_WRITABLE" != "0" ]; then
+        echo "==> Attempting to make storage directory writable"
+        chmod -R 777 storage
+    fi
+
+    if [ "$POST_INSTALL" ]; then
+        echo "==> Running POST_INSTALL"
+        sh -c "$POST_INSTALL"
+    fi
 
 fi
